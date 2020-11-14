7dede228a7d9521daae58ce4b5bd418e35474285
axetroy <axetroy.dev@gmail.com>
2020-11-15 03:08:43 +0800

    v0.2.2

--- main.v
+++ main.v
@@ -5,7 +5,7 @@ import sync { RwMutex }
 import flag
 
 const (
-	version    = 'v0.2.1'
+	version    = 'v0.2.2'
 	dir_prune  = ['node_modules', 'bower_components', '.temp', '.dist']
 	file_prune = ['.DS_Store']
 )
